35f61c03623165e1c4fb4c6b708f38d6babe7fd3
Jessica Nygren <jessicanygren@C02D54BJMD6R.lan>
2021-11-07 22:10:02 +0100

    code along with Damien

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { game } from 'reducers/game'
 import { Player } from 'components/Player'
 
 export const Stats = () => {
+  const dispatch = useDispatch()
   // TODO - get the 'winner' from the game reducer
-  const winner = null
+  const winner = useSelector((store) => store.game.winner)
   // TODO - get the 'nextPlayer' from the game reducer
-  const nextPlayer = 'x'
+  const nextPlayer = useSelector((store) => store.game.player)
 
   const handleButtonPress = () => {
+    dispatch(game.actions.restart())
     // TODO - dispatch the restart action
   }
 
